82f63e8d9c7488f296892d0353a1e52ba2488e54
fahad1226 <54905485+AjAsif49@users.noreply.github.com>
2022-02-12 23:42:30 +0600

    first

---
+++ hello.js
@@ -0,0 +1,18 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+
+var age = 21;
+console.log(typeof age);
+
+var name = "Asif Jalal";
+console.log(name);
+
+var isHot = true;
+var isRich = false;
+console.log(isHot);
+console.log(typeof isRich);
+
+
+var myName = 'Steve Smith';
+myName = 'De viliers';
+console.log(myName);
