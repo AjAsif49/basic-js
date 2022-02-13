d8a4766251d4321f48e20983d4fa75b16870e497
fahad1226 <54905485+AjAsif49@users.noreply.github.com>
2022-02-13 20:54:24 +0600

    end

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+var price = 17;
+var choice = true;
+if(price < 15 && choice == true ){
+    console.log('I will buy this');
+}
+else if(choice){
+    console.log('I will buy this');
+}
+else{
+    console.log("I won't buy this");
+}
--- hello.js
+++ hello.js
@@ -16,3 +16,36 @@ console.log(typeof isRich);
 var myName = 'Steve Smith';
 myName = 'De viliers';
 console.log(myName);
+
+
+var promise = "I promise that I will work HARD from tomorrow";
+console.log(promise.toUpperCase());
+console.log(promise.indexOf('will'));
+
+var number1 = 20;
+var number2 = '20.5';
+number2 = parseFloat(number2);
+console.log(number1 + number2);
+
+
+var num1 = 0.1;
+var num2 = 0.2;
+var total = num1 + num2;
+total = total.toFixed(1)
+console.log(total);
+
+
+var price1 = 25;
+var price2 = 35;
+price2++;
+console.log(price2);
+
+var number = 5.49999;
+// var result = Math.round(number)
+// var result = Math.ceil(number);
+var result = Math.floor(number);
+console.log(result)
+
+
+var date = new Date();
+console.log(date);
